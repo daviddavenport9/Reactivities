718f8493f87425d1234a11c6cfc5044c01d2d8e0
daviddavenport9 <daviddavenport43@gmail.com>
2022-04-14 12:04:06 -0400

    Added real time comments with SignalR

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Domain;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence
@@ -19,6 +18,7 @@ namespace Persistence
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -28,6 +28,8 @@ namespace Persistence
             builder.Entity<ActivityAttendee>().HasOne(u => u.AppUser).WithMany(a => a.Activities).HasForeignKey(aa => aa.AppUserId);
 
             builder.Entity<ActivityAttendee>().HasOne(u => u.Activity).WithMany(a => a.Attendees).HasForeignKey(aa => aa.ActivityId);
+
+            builder.Entity<Comment>().HasOne(a => a.Activity).WithMany(b => b.Comments).OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
