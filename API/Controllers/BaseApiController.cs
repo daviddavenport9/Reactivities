098187cbf374a180cf9b772ecf89bfb5fddb21a1
daviddavenport9 <daviddavenport43@gmail.com>
2022-04-15 13:26:58 -0400

    Pagination, filtering, and sorting for activities

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Application.Core;
+using API.Extensions;
 
 namespace API.Controllers
 {
@@ -25,6 +26,25 @@ namespace API.Controllers
 
             if (result.isSuccess && result.Value != null)
             {
+                return Ok(result.Value);
+            }
+            if (result.isSuccess && result.Value == null)
+            {
+                return NotFound();
+            }
+            return BadRequest(result.Error);
+        }
+
+           protected ActionResult HandlePagedResult<T>(Result<PagedList<T>> result)
+        {
+
+            if (result == null ){
+                return NotFound();
+            }
+
+            if (result.isSuccess && result.Value != null)
+            {
+                Response.AddPaginationHeader(result.Value.CurrentPage, result.Value.PageSize, result.Value.TotalCount, result.Value.TotalPages);
                 return Ok(result.Value);
             }
             if (result.isSuccess && result.Value == null)
